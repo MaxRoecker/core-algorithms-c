e75f66029bb57be9186697d42fe8f15c6e6be821
MaxRoecker <max.roecker@gmail.com>
2016-12-26 22:13:06 -0200

    Add comparison module

---
+++ src/commons/comparison.h
@@ -0,0 +1,25 @@
+#ifndef COMPARISON_H
+#define COMPARISON_H
+
+/**
+  A type of a function that returns a basic comparison between two elements.
+
+  A basic comparison must return:
+    * 1, if the first element is greater than the second;
+    * 0, if the first element is equals to the second;
+    * -1, if the first element is smaller than the second;
+*/
+typedef char (*basic_comparison)(void * const, void * const);
+
+
+/**
+  A type of a function that returns a distance between two elements.
+
+  If the distance is:
+    * zero, the elements are equal;
+    * positive, the first element is greater than the second;
+    * negative, the first element is smaller than the second;
+*/
+typedef double (*distance_comparison)(void * const, void * const);
+
+#endif
