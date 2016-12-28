a643e32e7f8182c09850a37fba596c7da53a0ea3
MaxRoecker <max.roecker@gmail.com>
2016-12-28 16:51:13 -0200

    Add some tests

--- tests/structures_array_tests.c
+++ tests/structures_array_tests.c
@@ -57,41 +57,51 @@ void test_copy() {
 }
 
 void test_slice() {
-  // 3
+  // 5
   void *a[] = {&zero, &one, &two, &three, &four};
-  void *b[] = {&zero, &one, &two};
-  void *c[] = {&one, &two, &three};
+  void *b[] = {&zero, &one};
+  void *c[] = {&two};
   void *d[] = {&three, &four};
+  void *e[] = {};
   Array complete = array_create_from(a, 5);
-  Array prefix = array_create_from(b, 3);
-  Array infix = array_create_from(c, 3);
+  Array prefix = array_create_from(b, 2);
+  Array infix = array_create_from(c, 1);
   Array suffix = array_create_from(d, 2);
+  Array empty = array_create_from(e, 0);
 
-  Array slice_prefix = array_slice(complete, 0, 3);
-  Array slice_infix = array_slice(complete, 1, 4);
+  Array slice_prefix = array_slice(complete, 0, 2);
+  Array slice_infix = array_slice(complete, 2, 3);
   Array slice_suffix = array_slice(complete, 3, 5);
+  Array slice_empty = array_slice(complete, 5, 5);
+  Array slice_complete = array_slice(complete, 0, 5);
 
   ok(array_equals(prefix, slice_prefix) == 1, "Must be prefix.");
   ok(array_equals(infix, slice_infix) == 1, "Must be infix.");
   ok(array_equals(suffix, slice_suffix) == 1, "Must be suffix.");
+  ok(array_equals(empty, slice_empty) == 1, "Must be empty.");
+  ok(array_equals(complete, slice_complete) == 1, "Must be equals.");
 
   array_destroy(&complete);
   array_destroy(&prefix);
   array_destroy(&infix);
   array_destroy(&suffix);
+  array_destroy(&empty);
   array_destroy(&slice_prefix);
   array_destroy(&slice_infix);
   array_destroy(&slice_suffix);
+  array_destroy(&slice_empty);
+  array_destroy(&slice_complete);
 }
 
 void test_merge() {
-  // 2
+  // 3
   void *a[] = {&zero, &one, &two, &three, &four};
   void *b[] = {&zero, &two, &four};
   void *c[] = {&one, &three};
   Array complete = array_create_from(a, 5);
   Array one = array_create_from(b, 3);
   Array another = array_create_from(c, 2);
+  Array empty = array_create_empty(0);
   ComparisonFunction comparison = &comparison_as_integer;
 
   Array one_merge = array_merge(one, another, comparison);
@@ -100,13 +110,20 @@ void test_merge() {
   Array another_merge = array_merge(another, one, comparison);
   ok(array_equals(another_merge, complete) == 1, "Arrays must be equals.");
 
+  Array complete_merge = array_merge(empty, complete, comparison);
+  ok(array_equals(complete_merge, complete) == 1, "Arrays must be equals.");
+
   array_destroy(&complete);
   array_destroy(&one);
   array_destroy(&another);
+  array_destroy(&empty);
+  array_destroy(&one_merge);
+  array_destroy(&another_merge);
+  array_destroy(&complete_merge);
 }
 
 int main() {
-  plan(29 + 1 + 3 + 2);
+  plan(29 + 1 + 5 + 3);
 
   test_creation_destruction();
   test_copy();
